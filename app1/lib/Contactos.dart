a3a9caa35967d3c24aac073f9181600b7ce5b038
nvelezunilasallista <nvelez@unilasallista.edu.co>
2025-08-26 10:56:17 -0500

    App 1: Stateless

---
+++ app1/lib/Contactos.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+
+class Contactos extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: new AppBar(title: new Text("Ejemplo 1")),
+      body: ListView(children: [
+      Column(
+        children: [
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          ),
+          Card(
+            child:Column(
+              children:[
+                Text("Nombre Contacto"),
+                Text("3001345689")
+              ]
+            )
+          )
+        ],
+      )
+      ],)
+    );
+    
+  }
+
+  
+}
---
+++ app1/lib/main.dart
@@ -0,0 +1,3 @@
+import 'package:app1/Contactos.dart';
+import 'package:flutter/material.dart';
+void main() { runApp(MaterialApp(home: Contactos()));}
